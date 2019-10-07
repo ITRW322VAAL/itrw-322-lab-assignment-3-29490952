70d8d129ddfa31f20c50314c1ca9f702f6732b74
29490952 <thatoarie17@gmail.com>
2019-10-07 20:45:39 +0200

    29490952 - code added to server.py

---
+++ LAB3/29490952_Sockets/Server.py
@@ -0,0 +1,18 @@
+#!/user/bin/env python3
+
+import socket
+
+HOST = "127.0.0.1"
+PORT = 9999
+
+with socket.socket(socket.AF.INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Server is connected with address', addr)
+        while True:
+            data = conn.recv(2048)
+            if not data:
+                break
+            conn.sendall(data)
